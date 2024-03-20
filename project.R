bf8431240f1fc501f12376a1d062e3704651a448
matthew-townson <mjwtownson@gmail.com>
2024-03-20 19:51:06 +0000

    uses subset
    Dataset adjusted before plotting instead of after

--- project.R
+++ project.R
@@ -1,5 +1,5 @@
 # make sure to set working directory to where the files are stored
-setwd("~/projects/104-group-project/") # this may not work for your system
+setwd("~/projects/matthew-townson/104-group-project/") # this may not work for your system
 
 # Install required packages
 install.packages('ggplot2')
@@ -12,20 +12,21 @@ install.packages('rnaturalearth')
 library(ggplot2)
 library(dplyr)
 library(lubridate)
-library(rnaturalearth)
 
 # Import data
 nydata <- read.csv("./data/merged.csv")
 
 # making a subset of data that only includes the island of manhattan
-nyc <- subset(nydata, (-73.5>=nydata$Lon>=-74.2 | 40.55>=nydata$Lat>=40.94375) )
+nyc <- subset(nydata, (-74.2>=nydata$Lon & nydata$Lon<=-73.5) | (41>=nydata$Lat & nydata$Lat>=40.5) )
+# * Filter that didn't work :: nyc <- nyc %>% filter((-74.6>=nydata$Lon & nydata$Lon>=-73) & (41.45>=nydata$Lat & nydata$Lat>=40.55
 
 # View raw data
 View(nydata)
+View(nyc)
 
 # Plot graph
 # PLAN - plot this data as a scatter and overlay with a map
-plot(nydata$Lon, nydata$Lat, pch=19, col="black")
+# * OLD plot :: plot(nydata$Lon, nydata$Lat, pch=19, col="black")
 
 # Plots ALL - shows all of the pickups across the state
 ggplot(nydata, aes(x=Lon, y=Lat) ) +
@@ -35,8 +36,9 @@ ggplot(nydata, aes(x=Lon, y=Lat) ) +
 ggsave("./exported/full.png", width = 16, height = 9, device="png", dpi=1600)
 
 # Plots only NYC area - do between x(-74.2, -73.5) and y(40.55, 40.94375)
-ggplot(nydata, aes(x=Lon, y=Lat) ) +
-    geom_bin2d(bins=15000) +
+ggplot(nyc, aes(x=Lon, y=Lat) ) +
+    geom_bin2d(bins=3000) +
     scale_fill_continuous(type = "viridis") +
     theme_bw()
+ggsave("./exported/nyc.png", device="png", dpi=2000)
 ggsave("./exported/nyc.png", width = 16, height = 9, device="png", dpi=2000)
